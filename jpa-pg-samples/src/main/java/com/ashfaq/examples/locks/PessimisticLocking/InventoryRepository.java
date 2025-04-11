7cb4dbba58a1b963e0834be284960f98e63ec5e6
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2025-04-11 20:24:46 +0530

    Repo for  Pessimistic Locking

---
+++ jpa-pg-samples/src/main/java/com/ashfaq/examples/locks/PessimisticLocking/InventoryRepository.java
@@ -0,0 +1,16 @@
+package com.ashfaq.examples.locks.PessimisticLocking;
+
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<InventoryItem, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT i FROM InventoryItem i WHERE i.id = :id")
+    Optional<InventoryItem> findByIdForUpdate(@Param("id") Long id);
+}
